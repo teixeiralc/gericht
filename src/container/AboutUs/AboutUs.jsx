3ca23db878eae49015ec13585c66589e8aff7e0c
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-12-13 10:52:26 -0300

    add about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import styles from './AboutUs.module.css';
+import images from '../../constants/images';
 
-import './AboutUs.module.css';
+const AboutUs = () => {
+  return (
+    <section
+      id="about"
+      className={`${styles.about} app__bg flex__center section__padding`}
+    >
+      <div className={`${styles.overlay} flex__center`}>
+        <img src={images.G} alt="G letter" />
+      </div>
+      <div className={`${styles.content} flex__center`}>
+        <div className={styles.content_about}>
+          <h2 className="headtext__cormorant">About Us</h2>
+          <img src={images.spoon} alt="Spoon" />
+          <p className={styles.about_info}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+            necessitatibus eum, nemo temporibus quas a consequuntur voluptate
+            voluptatum iusto sed nesciunt dolore! Cumque illo dolores
+            consectetur consequatur pariatur itaque dolorem!
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
 
-const AboutUs = () => <div>AboutUs</div>;
+        <div className={styles.knife}>
+          <div className="flex__center">
+            <img src={images.knife} alt="Knife" />
+          </div>
+        </div>
+
+        <div className={styles.content_history}>
+          <h2 style={{ lineHeight: '1' }} className="headtext__cormorant">
+            Our History
+          </h2>
+          <img src={images.spoon} alt="Spoon" />
+          <p className={styles.history_info}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
+            necessitatibus eum, nemo temporibus quas a consequuntur voluptate
+            voluptatum iusto sed nesciunt dolore! Cumque illo dolores
+            consectetur consequatur pariatur itaque dolorem!
+          </p>
+          <button type="button" className="custom__button">
+            Know More
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default AboutUs;
--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
-import './Hero.module.css';
+import styles from './Hero.module.css';
+import images from '../../constants/images';
+import { SubHeading } from '../../components';
 
-const Hero = () => <div>Hero</div>;
+const Hero = () => (
+  <section className={`${styles.hero} app__wrapper section__padding`} id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase The New Flavor" />
+      <h1 className={styles.title}>
+        The Key To
+        <br /> Fine Dining
+      </h1>
+      <p className={styles.info_desc}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus{' '}
+      </p>
+      <button className="custom__button" type="button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="hero img" />
+    </div>
+  </section>
+);
 
 export default Hero;
